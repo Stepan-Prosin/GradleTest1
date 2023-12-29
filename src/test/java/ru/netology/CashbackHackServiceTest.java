577a9d21f7f3976011590b74e14030795cef3844
stepan TeSt <stepan.prosin@mail.ru>
2023-12-29 22:37:37 +0300

    part three mistake

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -8,9 +8,15 @@ public class CashbackHackServiceTest {
   @Test
     public  void remainTest(){
         CashbackHackService cash= new CashbackHackService();
-        int expected = 1000;
+        int expected = 700;
+        int actual = cash.remain(300);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public  void wrongRemainTest(){
+        CashbackHackService cash= new CashbackHackService();
+        int expected = 0;
         int actual = cash.remain(1000);
         Assert.assertEquals(expected,actual);
     }
-
 }
